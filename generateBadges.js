2dceca24105d72958d5da4f2172473292d63e441
Spazcool <douglas.wrightiii@gmail.com>
2020-05-30 23:08:16 -0400

    readme

--- generateBadges.js
+++ generateBadges.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-// const { makeBadge, ValidationError } = require('badge-maker')
 
 function getFileTypes(path){
     let arr = fs.readdirSync(path)
@@ -18,24 +17,6 @@ function getFileTypes(path){
 }
 
 // for each file type, generate a badge, with a percentage of rep of the directory
-// function assignTypesToBadge(files){
-//     let arr = [];
-//     let obj = {};
-//     let total = files.length;
-
-//     files.forEach((file) => { obj[file] = (obj[file] || 0) + 1; });
-
-//     for(type in obj){
-//         let format = {
-//             label: `${type}`,
-//             message: `${Math.floor((obj[type]/total) * 100)}%  `,
-//             color: `#${Math.floor(Math.random()*16777215).toString(16)}`,
-//         }
-//         arr.push(makeBadge(format))
-//     }
-
-//     return arr.join(" ");
-// }
 function assignTypesToBadge(files){
     let arr = [];
     let obj = {};
@@ -44,12 +25,9 @@ function assignTypesToBadge(files){
     files.forEach((file) => { obj[file] = (obj[file] || 0) + 1; });
 
     for(type in obj){
-        let format = `[![Generic badge](https://img.shields.io/badge/${type}-${Math.floor((obj[type]/total) * 100)}%-red.svg)](https://shields.io/)`;
-        // {
-        //     label: `${type}`,
-        //     message: `${Math.floor((obj[type]/total) * 100)}%  `,
-        //     color: `#${Math.floor(Math.random()*16777215).toString(16)}`,
-        // }
+        let color = Math.floor(Math.random()*16777215).toString(16);
+        let percent = Math.floor((obj[type]/total) * 100);
+        let format = `[![Generic badge](https://img.shields.io/badge/${type}-${percent}%25%0D%0A-${color}.svg)](https://shields.io/)`;
         arr.push(format)
     }
 
--- readmes/test/README.md
+++
@@ -1,8 +0,0 @@
-# test
-[![Generic badge](https://img.shields.io/badge/.js-42%-red.svg)](https://shields.io/) [![Generic badge](https://img.shields.io/badge/.json-28%-red.svg)](https://shields.io/) [![Generic badge](https://img.shields.io/badge/.md-14%-red.svg)](https://shields.io/) [![Generic badge](https://img.shields.io/badge/.yml-14%-red.svg)](https://shields.io/)
----
-## Table of Contents
-1. [title](#title)
-2. [contributors](#contributors)
-## Contributors
-<a href="https://github.com/Spazcool"><img src="https://avatars2.githubusercontent.com/u/17243640?v=4" title="Spazcool" width="10%"/></a> <a href="https://github.com/Sammon123"><img src="https://avatars3.githubusercontent.com/u/61717956?v=4" title="Sammon123" width="10%"/></a> <a href="undefined"><img src="undefined" title="undefined" width="10%"/></a> <a href="https://github.com/89"><img src="https://avatars3.githubusercontent.com/u/34496404?v=4" title="89" width="10%"/></a>
